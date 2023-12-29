8272fde734dffc0aaa85ae88222b19a147be776d
Li Yiming <53037177+TaylorLeeSwift@users.noreply.github.com>
2023-12-30 00:31:31 +0800

    Add files via upload

--- application.py
+++ application.py
@@ -1,13 +1,8 @@
 import json
 from math import radians, sin, cos, sqrt, atan2
 from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans
 import time
-from sqlalchemy.orm import Session
-from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -15,37 +10,6 @@ app = Flask(__name__)
 city_data = pd.read_csv('us-cities.csv')
 
 review_data = pd.read_csv('newre.csv')
-
-# 替换下面的数据库连接字符串为你的MySQL数据库连接信息
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:lym222@112.126.68.6:3307/Cloud?charset=utf8'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 设置为False以避免追踪修改导致性能问题
-
-db = SQLAlchemy(app)
-# 创建 Session
-# db = pymysql.connect(host="112.126.68.6",
-#                      user="root",
-#                      password="lym222",
-#                      database="Cloud",
-#                      port=3307)
-
-# Define your model class for the 'reviews' table
-class Reviews(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    score = db.Column(db.Text)
-    city = db.Column(db.Text)
-    title = db.Column(db.Text)
-    review = db.Column(db.Text)
-    cluster = db.Column(db.Text)
-
-
-class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.Text)
-    lat = db.Column(db.Text)
-    lng = db.Column(db.Text)
-    country = db.Column(db.Text)
-    state = db.Column(db.Text)
-    population = db.Column(db.Integer)
 
 # 从数据库获取评论文本数据
 # 每页城市数
@@ -162,120 +126,6 @@ def index_review():
         return render_template('index_review.html', data=data)
 
     return render_template('index_review.html', data=None)
-
-
-@app.route('/index_knn', methods=['GET', 'POST'])
-def index_knn():
-    return render_template('index_knn.html')
-
-@app.route('/knn_reviews', methods=['POST'])
-def knn_reviews():
-    start_time = time.time()
-
-    # 获取请求参数
-    classes = int(request.form.get('classes', 3))
-    k = int(request.form.get('k', 3))
-    words = int(request.form.get('words', 5))
-
-    # 从数据库获取评论文本数据
-    with app.app_context():
-        reviews = [review.review for review in Reviews.query.limit(300).all()]
-
-    # 使用TF-IDF向量化文本数据
-    vectorizer = TfidfVectorizer(stop_words='english')
-    X = vectorizer.fit_transform(reviews)
-
-    # 使用KMeans进行聚类
-    kmeans = KMeans(n_clusters=classes, random_state=42)
-    clusters = kmeans.fit_predict(X)
-
-    # 更新数据库表中的cluster列
-    for idx, cluster_id in enumerate(clusters):
-        review = Reviews.query.get(idx + 1)  # Assuming the reviews are indexed starting from 1
-        review.cluster = cluster_id
-        db.session.commit()
-
-    # 处理每个聚类并准备响应
-    results = []
-    for cluster_id in range(classes):
-        cluster_reviews = Reviews.query.filter_by(cluster=cluster_id).limit(300).all()
-
-        result = {
-            'class_id': cluster_id,
-            'cluster_reviews': [review.review for review in cluster_reviews],
-            'popular_words': get_popular_words([review.review for review in cluster_reviews], words),
-        }
-
-        results.append(result)
-
-    response_time = int((time.time() - start_time) * 1000)  # 计算响应时间（毫秒）
-
-    # 创建一个字典用于存储每个 cluster 对应的 population 之和
-    cluster_population = {}
-
-    # 创建一个 Session
-    session = Session(db)
-
-    with app.app_context():
-        temps = [(review.city, review.cluster) for review in Reviews.query.limit(300).all()]
-    # print(reviews)
-
-    with app.app_context():
-        cities = [(city.city, city.population) for city in City.query.all()]
-    # print(cities)
-
-    # 创建一个字典，用于存储每个cluster的population总和
-    cluster_population = defaultdict(int)
-
-    # 遍历temps数组，将相同cluster的城市的population相加
-    for city, cluster in temps:
-        for c, population in cities:
-            if city == c:
-                cluster_population[cluster] += population
-
-    # 将字典转换为列表形式
-    result = [[cluster, population] for cluster, population in cluster_population.items()]
-
-    print(result)
-
-    # return render_template('knn_result.html', results=results, response_time=response_time)
-    return render_template('show.html', data_from_backend=result)
-
-def get_popular_words(reviews, num_words):
-    # 使用简单的示例逻辑获取最受欢迎的单词
-    # 在实际应用中，您可能需要使用更复杂的NLP技术
-    words = ' '.join(reviews).split()
-    # word_freq = pd.Series(words).value_counts()
-    # popular_words = word_freq.head(num_words).index.tolist()
-    stop_word = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself',
-                 'yourselves',
-                 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
-                 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which',
-                 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is',
-                 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did',
-                 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or''because', 'as', 'until',
-                 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during',
-                 'before', 'after', 'above', 'below', 'to', 'from', 'up',
-                 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there',
-                 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each',
-                 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
-                 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don',
-                 'should', 'now']
-    # 对评论进行分词，并统计单词出现的次数
-    word_counts = {}
-    for review in words:
-        for word in review.split():
-            if word not in stop_word:
-                if word in word_counts:
-                    word_counts[word] += 1
-                else:
-                    word_counts[word] = 1
-    word_list = []
-    top_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:num_words]  # 获取最常见的十个单词及出现次数
-    for word, count in top_words:
-        word_list.append(word)
-    return word_list
-
 
 
 
